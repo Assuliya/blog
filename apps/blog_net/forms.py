ea162d55015d4026c428ac394c7b2cd1a241cc99
Assuliya <t.nesterchuk@mail.ru>
2016-08-31 17:31:07 +0300

    custom form validations

--- apps/blog_net/forms.py
+++ apps/blog_net/forms.py
@@ -5,3 +5,27 @@ class ContactForm(forms.Form):
     email = forms.EmailField()
     title = forms.CharField()
     text = forms.CharField(widget=forms.Textarea)
+
+    def clean_title(self):
+        cd = self.cleaned_data
+        title = cd.get('title')
+        if len(title) < 3:
+            raise forms.ValidationError("Title should be more than two characters")
+        return title
+
+    def clean_text(self):
+        cd = self.cleaned_data
+        text = cd.get('text')
+        if len(text) < 10:
+            raise forms.ValidationError("Please type in some more text")
+        return text
+
+    def clean(self):
+        cd = self.cleaned_data
+
+        email = cd.get('email')
+        title = cd.get('title')
+
+        if email == title:
+            raise forms.ValidationError("Title should not be an email")
+        return cd
