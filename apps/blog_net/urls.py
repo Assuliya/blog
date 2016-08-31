1d0c4e7bdc4258038179cb318005b4cd88fd417c
Assuliya <t.nesterchuk@mail.ru>
2016-08-31 19:09:34 +0300

    rss feed created

--- apps/blog_net/urls.py
+++ apps/blog_net/urls.py
@@ -1,9 +1,16 @@
 from django.conf.urls import url
 from . import views
+from feeds import ArchiveFeed
+
+
 urlpatterns = [
     url(r'^$', views.index, name='index'),
     url(r'^about/$', views.about, name='about'),
     url(r'^archive/$', views.archive, name='archive'),
     url(r'^contact/$', views.contact, name='contact'),
 
+]
+
+urlpatterns += [
+    url(r'^feed/archive/$', ArchiveFeed()),
 ]
